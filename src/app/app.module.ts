d90afa7e502949911974f5e922e2585a847bdb2c
JuanSCaicedo <sebanchit@gmail.com>
2024-04-30 21:48:21 -0500

    usando modulos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { WelcomeRoutingModule } from './welcome/welcome-routing.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    WelcomeRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
